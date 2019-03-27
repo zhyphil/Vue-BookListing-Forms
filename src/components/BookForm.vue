fcbc5ec8962c502b6e4d68e4e8adfcbb43846257
Haoyu ZUO <zhyphil@gmail.com>
2019-03-27 11:53:20 +0100

    Adding a Search Functionality

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,25 +1,27 @@
 <template>
-<form v-on:submit.prevent="bookSubmit(bookData)" class="" action="#" method="post">
-  <input v-model="bookData.bookTitle" type="text" name="title" value="" placeholder="Book Title">
-  <input v-model="bookData.bookAuthor" type="text" name="author" value="" placeholder="Book Author">
-  <div>
-  <label for="finishedReading">
-  Finished Reading
-    <input type="checkbox" v-model="bookData.finishedReading">
-  </label>
-  </div>
-  <div>
-    <label for="borrowed">
-    borrowed
-      <input type="radio" value="borrowed" v-model="bookData.ownership">
-    </label>
-    <label for="bought">
-    bought
-      <input type="radio" value="bought" v-model="bookData.ownership">
-    </label>
-  </div>
-  <button type="submit" name="button">Add Book</button>
-</form>
+  <form v-on:submit.prevent="bookSubmit(bookData)" class="" action="#" method="post">
+    <input v-model="bookData.bookTitle" type="text" name="title" value="" placeholder="Book Title">
+    <input v-model="bookData.bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+    <div>
+      <div>
+        <label for="finishedReading">
+          Finished Reading
+          <input v-model="bookData.finishedReading" type="checkbox" name="finishedReading" value="">
+        </label>
+      </div>
+    </div>
+    <div>
+      <label for="borrowed">
+        borrowed
+        <input v-model="bookData.ownership" type="radio" id="borrowed" value="borrowed">
+      </label>
+      <label for="bought">
+        bought
+        <input v-model="bookData.ownership" type="radio" id="bought" value="bought">
+      </label>
+    </div>
+    <button type="submit" name="button">Add Book</button>
+  </form>
 </template>
 
 <script>
@@ -32,7 +34,7 @@ export default {
         bookTitle: "",
         bookAuthor: "",
         finishedReading: false,
-        ownership:[]
+        ownership: []
       }
     };
   },
--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,64 +1,71 @@
 <template>
-<div>
-  <h1>{{title}}</h1>
-  <ul class="exists">
-    <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
-  </ul>
-  <hr>
-  <h2>Filtered Books by Ownership</h2>
-  <select v-model="holding">
-    <option v-for="filter in filters">{{filter}}</option>
-  </select>
-  <ul class="exists">
-    <book-item v-for='book in filteredBooks' :key='book.id' :book='book'></book-item>
-  </ul>
-  <br>
-  <hr>
-  <book-form @addBook='appendBook'></book-form>
-</div>
+  <div>
+    <h1>{{title}}</h1>
+    <input type="text" v-model="searchInput" placeholder="Search Books">
+    <ul class="exists">
+      <book-item v-for='book in searchedBooks' :key='book.id' :book='book'></book-item>
+    </ul>
+    <hr>
+    <h2>Filtered Books by Ownership</h2>
+    <select v-model="holding">
+      <option v-for="filter in filters">{{ filter }}</option>
+    </select>
+    <ul class="exists">
+      <book-item v-for='book in filteredBooks' :key='book.id' :book='book'></book-item>
+    </ul>
+    <br><hr>
+    <book-form @addBook='appendBook'></book-form>
+  </div>
 </template>
 
 <script>
-import _ from "lodash"
+import _ from "lodash";
 import BookItem from "./BookItem";
 import BookForm from "./BookForm";
-
 export default {
   name: "BookList",
   data() {
     return {
       title: "All Books",
       states: ["Want to Read", "Read", "Reading"],
-      books: [{
+      books: [
+        {
           title: "Self-Reliance",
           author: "Ralph Waldo Emerson",
           finishedReading: true,
-          ownership:"borrowed"
+          ownership: "borrowed"
         },
         {
           title: "American Gods",
           author: "Neil Gaiman",
           finishedReading: false,
-          ownership:"bought"
+          ownership: "bought"
         },
         {
           title: "Amusing Ourselves to Death",
           author: "Neil Postman",
           finishedReading: true,
-          ownership:"borrowed"
+          ownership: "borrowed"
         }
       ],
-      filters: ["bought","borrowed"],
-      holding:"bought"
+      filters: ["bought", "borrowed"],
+      holding: "bought",
+      searchInput: ""
     };
   },
   components: {
     BookItem,
     BookForm
   },
-  computed:{
-    filteredBooks(){
-    return _.filter(this.books, ["ownership", this.holding])
+  computed: {
+    filteredBooks() {
+      return _.filter(this.books, ["ownership", this.holding]);
+    },
+    searchedBooks() {
+      const searchFilter = book => {
+        return book.title.toLowerCase().match(this.searchInput.toLowerCase());
+      };
+      return _.filter(this.books, searchFilter);
     }
   },
   methods: {
@@ -79,12 +86,10 @@ h1,
 h2 {
   font-weight: normal;
 }
-
 ul {
   list-style-type: none;
   padding: 0;
 }
-
 li {
   display: block;
   margin: 0 10px;
